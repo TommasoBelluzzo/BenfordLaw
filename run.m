f41d858d6494f53ddf129d1508f585f729932a31
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2018-05-29 00:58:43 +0200

    Add files via upload

--- run.m
+++ run.m
@@ -8,10 +8,10 @@ clc();
 paths = genpath(path);
 addpath(paths);
 
-%imp = importdata('\Datasets\Example.xlsx');
-%data = imp.data;
+imp = importdata('\Datasets\Example.xlsx');
+data = imp.data;
 
-data = importdata('\Datasets\CP.xlsx');
+%data = importdata('\Datasets\CP.xlsx');
 %data = benford_random([1000 1],50000);
 
 benford_analyse(data);
